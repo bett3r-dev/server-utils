dbf62a241fc2655f42c3bd0b2b2b4c348d43206d
Tomás Ruiz <tomasruizr@gmail.com>
2021-03-18 02:21:51 -0300

    [add] tests

---
+++ src/modules/tests.ts
@@ -0,0 +1,26 @@
+import {map} from 'rambda';
+import http from 'http';
+
+export interface fakeResponse {
+  code?: number
+  route?: string
+  body?: any
+}
+
+/**
+ * Creates a test http server. It will automatically listen on a random port
+  * @param payloadToReturn Payload to return to all possible requests or an array of pairs with path and payload
+ * @returns {http.Server}
+ */
+export const createFakeServer = ( payloadToReturn?: fakeResponse[] | fakeResponse ): http.Server =>
+  http.createServer(( req, res ) => {
+    map(( value, key ) => res.setHeader( key, value ), req.headers );
+    let route: fakeResponse = {};
+    if ( Array.isArray( payloadToReturn ))
+      route = payloadToReturn.find( r => new RegExp( r.route ).test( req.url ));
+    else
+      route = payloadToReturn;
+    const body = route && route.body;
+    res.statusCode = route && route.code || 200;
+    res.end( JSON.stringify( body ));
+  }).listen();
