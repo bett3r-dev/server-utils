60ce04ae6ff690124dccfad1ad63c9ef18722e2c
Tomás Ruiz <tomasruizr@gmail.com>
2021-03-18 02:22:00 -0300

    [add] timers

---
+++ src/modules/timers.test.ts
@@ -0,0 +1,84 @@
+import { assert } from 'chai';
+import sinon from "sinon";
+import {intervalAtEveryUTC} from './timers';
+
+describe( 'timers', function() {
+  describe( 'intervalAtEveryUTC', function() {
+    it( 'starts an interval each 24h from 00h', () => {
+      const fakeTimers = sinon.useFakeTimers();
+      const setTimeoutSpy = sinon.spy( fakeTimers, 'setTimeout' );
+      const setIntervalSpy = sinon.spy( fakeTimers, 'setInterval' );
+      intervalAtEveryUTC();
+      assert.equal( new Date( setTimeoutSpy.args[0][1]).getUTCHours(), 0 );
+      fakeTimers.next();
+      assert.equal( setIntervalSpy.args[0][1], 86400000 );
+      fakeTimers.restore();
+    });
+    it( 'starts an interval at specific hour', () => {
+      const fakeTimers = sinon.useFakeTimers();
+      const setTimeoutSpy = sinon.spy( fakeTimers, 'setTimeout' );
+      intervalAtEveryUTC({ startTime:'3' });
+      assert.equal( new Date( setTimeoutSpy.args[0][1]).getUTCHours(), 3 );
+      fakeTimers.restore();
+    });
+    it( 'starts an interval at specific minutes', () => {
+      const fakeTimers = sinon.useFakeTimers();
+      const setTimeoutSpy = sinon.spy( fakeTimers, 'setTimeout' );
+      intervalAtEveryUTC({ startTime: '00:34' });
+      assert.equal( new Date( setTimeoutSpy.args[0][1]).getMinutes(), 34 );
+      fakeTimers.restore();
+    });
+    it( 'starts an interval at specific seconds', () => {
+      const fakeTimers = sinon.useFakeTimers();
+      const setTimeoutSpy = sinon.spy( fakeTimers, 'setTimeout' );
+      intervalAtEveryUTC({ startTime: '02:33:34' });
+      assert.equal( new Date( setTimeoutSpy.args[0][1]).getSeconds(), 34 );
+      fakeTimers.restore();
+    });
+    it( 'creates an interval of the specified value', () => {
+      const fakeTimers = sinon.useFakeTimers();
+      const setIntervalSpy = sinon.spy( fakeTimers, 'setInterval' );
+      const setTimeoutSpy = sinon.spy( fakeTimers, 'setTimeout' );
+      intervalAtEveryUTC({ startTime:'3', interval: 60000 });
+      fakeTimers.tick( new Date( setTimeoutSpy.args[0][1]).valueOf());
+      assert.equal( setIntervalSpy.args[0][1], 60000 );
+      fakeTimers.restore();
+    });
+    it( 'creates an interval starting now', () => {
+      const fakeTimers = sinon.useFakeTimers();
+      const setIntervalSpy = sinon.spy( fakeTimers, 'setInterval' );
+      const setTimeoutSpy = sinon.spy( fakeTimers, 'setTimeout' );
+      intervalAtEveryUTC({ startTime:false, interval: 60000 });
+      assert.isTrue( setTimeoutSpy.notCalled );
+      assert.equal( setIntervalSpy.args[0][1], 60000 );
+      fakeTimers.restore();
+    });
+    it( 'starts the interval in the next starting time possible', () => {
+      const fakeTimers = sinon.useFakeTimers( new Date( '2020/01/01 06:01:00' ));
+      const setIntervalSpy = sinon.spy( fakeTimers, 'setInterval' );
+      const setTimeoutSpy = sinon.spy( fakeTimers, 'setTimeout' );
+      intervalAtEveryUTC({ startTime:'06:00', interval: 120000 });
+      assert.equal( setTimeoutSpy.args[0][1], 60000 );
+      fakeTimers.next();
+      assert.equal( setIntervalSpy.args[0][1], 120000 );
+      fakeTimers.restore();
+    });
+    it( 'stops an interval by ending the returning stream', () => {
+      const fakeTimers = sinon.useFakeTimers();
+      const setIntervalSpy = sinon.spy( fakeTimers, 'setInterval' );
+      const setTimeoutSpy = sinon.spy( fakeTimers, 'setTimeout' );
+      const s = intervalAtEveryUTC({ startTime:'3', interval: 1000 });
+      const mapSpy = sinon.spy();
+      s.map( mapSpy );
+      fakeTimers.tick( new Date( setTimeoutSpy.args[0][1]).valueOf());
+      assert.equal( setIntervalSpy.args[0][1], 1000 );
+      fakeTimers.next();
+      fakeTimers.next();
+      s.end( true );
+      fakeTimers.next();
+      fakeTimers.next();
+      fakeTimers.restore();
+      assert.equal( mapSpy.callCount, 3 );
+    });
+  });
+});
---
+++ src/modules/timers.ts
@@ -0,0 +1,34 @@
+import Stream from '@tomasruizr/flyd';
+
+export function intervalAtEveryUTC( params?: { startTime?: string|boolean; interval?: number; } ) {
+  params = params || {};
+  let hour , minute , second, intervalStart, hit;
+  const { startTime = '00:00:00', interval = 86400000 } = params;
+  const now = startTime === false;
+  const tick = Stream.stream();
+  const currentDate = new Date().valueOf();
+  let intervalHandler, timeoutHandler;
+  const executeInterval = () =>
+    setInterval(() => {
+      tick( new Date( new Date().valueOf() + interval ));
+    }, interval );
+
+  if ( !now ) {
+    [ hour = 0, minute = 0, second = 0 ] = (startTime as string).split( ':' );
+    intervalStart = ( Number( hour ) * 3600000 ) + ( Number( minute ) * 60000 ) + ( Number( second ) * 1000 );
+    hit = currentDate - ( currentDate % 86400000 ) + intervalStart;
+    while ( hit < currentDate ) hit += interval;
+    timeoutHandler = setTimeout(()=>{
+      tick( new Date( new Date().valueOf() + interval ));
+      intervalHandler = executeInterval();
+    }, hit - currentDate );
+  } else {
+    intervalHandler = executeInterval();
+  }
+  //close
+  tick.end.map(() => {
+    clearInterval( intervalHandler );
+    clearTimeout( timeoutHandler );
+  });
+  return tick;
+}
