c4a3bc628cdea8275dfa2759dac896fccef26b37
Tomás Ruiz <tomasruizr@gmail.com>
2021-03-18 02:19:05 -0300

    [add] initial commit

---
+++ wallaby.conf.js
@@ -0,0 +1,31 @@
+// const tsconfig = require( './tsconfig.json' );
+module.exports = function ( w ) {
+  process.env.NODE_ENV = 'test';
+  return {
+    compilers: {
+      '**/*.ts?(x)': w.compilers.typeScript({ isolatedModules: true, module:'commonjs'/* , useStandardDefaults: true */ })
+      // '**/*.ts?(x)': w.compilers.typeScript( tsconfig.compilerOptions )
+    },
+    files: [
+      'src/{,**}/*.ts',
+      'src/{,**}/*.js',
+      { pattern: 'src/{,**}/*.test.ts', ignore: true },
+      { pattern: 'wallaby.conf.js', ignore: true, instrument:false },
+    ],
+    filesWithNoCoverageCalculated:[
+      'src/fixtures/{,**}/*.ts',
+      'src/fixtures/{,**}/*.js',
+    ],
+    tests: [
+      'src/{,**}/*.test.ts',
+    ],
+    env: {
+      type: 'node'
+    },
+    runMode: 'onsave',
+    trace: true,
+    hints: {
+      ignoreCoverage: /istanbul ignore next/
+    },
+  };
+};
