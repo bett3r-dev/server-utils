d05832198bdd2f6aebac2839e12b3494e9576c82
Tomás Ruiz <tomasruizr@gmail.com>
2021-03-18 02:21:21 -0300

    [add] https

---
+++ src/modules/http.test.ts
@@ -0,0 +1,82 @@
+import { assert } from 'chai';
+import {createFakeServer} from './tests';
+import {fetchAsync, FetchResponse, processFetchResponse} from './http';
+import { Response } from 'node-fetch';
+import { AddressInfo } from 'node:net';
+
+describe( 'http', function() {
+  describe( 'fetchAsync', function() {
+    it( 'works like fetch but retuns an async', done => {
+      const server = createFakeServer();
+      fetchAsync( `http://localhost:${(server.address() as AddressInfo).port}` )
+        .fork( done, (response: FetchResponse) => {
+          assert.equal( response.status, 200 );
+          server.close(() => {
+            done();
+          });
+        });
+    });
+    it( 'sets the body in the response in the prop responseBody', done => {
+      const server = createFakeServer({body:{ name:'tomas' }});
+      fetchAsync( `http://localhost:${(server.address() as AddressInfo).port}` )
+        .fork( done, (response: Response) => {
+          let body = '';
+          response.body.on('data', chunk => {
+            body+= chunk
+          })
+          response.body.on('end', () => {
+            assert.equal( body, JSON.stringify({ name:'tomas' }));
+            server.close(() => {
+              done();
+            });
+          })
+        });
+    });
+  });
+  describe('processFetchJsonResponse', function() {
+    it('process the response from a fetch request when request is ok', done => {
+      const server = createFakeServer({body:{ name:'tomas' }});
+      fetchAsync( `http://localhost:${(server.address() as AddressInfo).port}` )
+        .chain(processFetchResponse)
+        .fork( done, (response: FetchResponse) => {
+          assert.deepEqual( response.body, { name:'tomas' });
+          server.close(() => {
+            done();
+          });
+        });
+    });
+    it('process the response from a fetch request when request is not ok and body is object', done => {
+      const server = createFakeServer({code:400, body:{ name:'tomas' }});
+      fetchAsync( `http://localhost:${(server.address() as AddressInfo).port}` )
+        .chain(processFetchResponse)
+        .fork((response: FetchResponse) => {
+          assert.deepEqual( response.body, { name:'tomas' });
+          server.close(() => {
+            done();
+          });
+        }, done);
+    });
+    it('process the response from a fetch request when request is not ok and body is text', done => {
+      const server = createFakeServer({code:400, body:"Error"});
+      fetchAsync( `http://localhost:${(server.address() as AddressInfo).port}` )
+        .chain(processFetchResponse)
+        .fork((response: FetchResponse<string>) => {
+          assert.equal( response.body, "Error");
+          server.close(() => {
+            done();
+          });
+        }, done);
+    });
+    it('process the response from a fetch request when request is not ok and body is number', done => {
+      const server = createFakeServer({code:400, body:234});
+      fetchAsync( `http://localhost:${(server.address() as AddressInfo).port}` )
+        .chain(processFetchResponse)
+        .fork((response: FetchResponse<number>) => {
+          assert.equal( response.body, 234);
+          server.close(() => {
+            done();
+          });
+        }, done);
+    });
+  });
+});
---
+++ src/modules/http.ts
@@ -0,0 +1,31 @@
+import { constant, isTrue } from 'crocks';
+import fetch, { RequestInit, Response } from 'node-fetch';
+import Async from 'crocks/Async';
+import safe from 'crocks/Maybe/safe';
+
+import {promiseToAsync} from './crocks'
+import { assoc, identity } from 'rambda';
+
+export interface FetchResponse<T = Record<string, any>> extends Omit<Response,'body'> {
+  body: T,
+}
+
+const parseResponseBody = (response: Response, method: 'reject'| 'resolve') =>
+   response.json()
+    .then( body => Promise[method](assoc('body', body, response)))
+
+export const processFetchResponse = (response: Response) =>
+  promiseToAsync(
+    safe(isTrue, response.ok)
+      .either(
+        () => parseResponseBody(response, 'reject'),
+        () => parseResponseBody(response, 'resolve'),
+      )
+  )
+
+export const fetchAsync = ( url: string, options?: RequestInit ): Async =>
+  promiseToAsync(fetch( url , {
+    method: 'get',
+    headers: { 'content-type': 'application/json; charset=utf-8' },
+    ...options,
+  }));
