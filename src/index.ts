c88d3c915b6872050f35982e9f3976000ece4d51
Tomás Ruiz <tomasruizr@gmail.com>
2021-03-24 14:34:27 -0300

    [add] export type declaration

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,11 @@ export * from './modules/modules'
 export * from './modules/process'
 export * from './modules/timers'
 export * from './modules/tests'
+
+declare type hashing = typeof import('./modules/hashing');
+declare type http = typeof import('./modules/http');
+declare type modules = typeof import('./modules/modules');
+declare type process = typeof import('./modules/process');
+declare type timers = typeof import('./modules/timers');
+declare type tests = typeof import('./modules/tests');
+export declare interface Utils extends hashing, http, modules, process, timers, tests{}
