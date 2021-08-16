25138631c427c062a7b5b4e0a02459d1d3d9d825
Tomás Ruiz <tomasruizr@gmail.com>
2021-08-16 15:15:39 -0300

    [update] package dependencies and publishing details

--- build/modules/crocks.d.ts
+++
@@ -1,5 +0,0 @@
-import Async from 'crocks/Async';
-export declare const promiseToAsync: (promise: Promise<any>) => Async;
-export declare const ensureAsync: (possibleAsync?: any) => any;
-export declare const readFile: import("crocks/internal").VariadicFunction<Async>;
-//# sourceMappingURL=crocks.d.ts.map
--- build/modules/crocks.test.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=crocks.test.d.ts.map
