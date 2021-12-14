a1aa8d67ce26979f5f167542c0bedd4638f2e37c
Tomas Ruiz <tomasruizr@gmail.com>
2021-12-14 00:42:45 -0300

    [update] nullaware schemas

--- src/modules/validation.ts
+++ src/modules/validation.ts
@@ -1,6 +1,7 @@
-import {Async, Result} from '@bett3r-dev/crocks';
+import {Async, isDefined, Result} from '@bett3r-dev/crocks';
 import { createError, EndpointActionParams, EndpointSchemas, ValidationError } from '@bett3r-dev/server-core';
 import joi from 'joi';
+import { safeAsync } from '..';
 
 export const validateResult = <T>(schema: joi.Schema<T>, options: joi.ValidationOptions = {abortEarly:false})=> (value:T): Result<T> => 
   Result.Ok(schema.validate(value, options))
@@ -9,17 +10,18 @@ export const validateResult = <T>(schema: joi.Schema<T>, options: joi.Validation
       : Result.Ok(result.value))  
 
 
-export const validateAsync = <T>(schema: joi.Schema<T>, options: joi.ValidationOptions = {abortEarly:false})=> (value:T) => 
-  Async.of(schema.validate(value, options))
+export const validateAsync = <T>(schema?: joi.Schema<T>, options: joi.ValidationOptions = {abortEarly:false})=> (value:T) => 
+  safeAsync(isDefined, schema)  
+    .map(() => schema.validate(value, options))
     .chain((result) => result.error
       ? Async.Rejected(createError(ValidationError, result.error.details)) 
       : Async.Resolved(result.value))  
 
 export const validateEndpoint = <T>(schemas: EndpointSchemas) => (epParams: EndpointActionParams<T>): Async<EndpointActionParams<T>> =>
   Async.of([
-    schemas.query ? validateAsync(schemas.query)(epParams.query) : Async.of(epParams.query),
-    schemas.params ? validateAsync(schemas.params)(epParams.params) : Async.of(epParams.params),
-    schemas.body ? validateAsync(schemas.body)(epParams.body) : Async.of(epParams.body)
+    schemas?.query ? validateAsync(schemas.query)(epParams.query) : Async.of(epParams.query),
+    schemas?.params ? validateAsync(schemas.params)(epParams.params) : Async.of(epParams.params),
+    schemas?.body ? validateAsync(schemas.body)(epParams.body) : Async.of(epParams.body)
   ])
     .chain(Async.all)
     .map<any>(([query, params, body])=>({
